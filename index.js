6f321c53f2470601fae6e75297b8fa4a5a0e01f3
SaharSuleman <qazirameezahmed569@gmail.com>
2020-10-29 12:30:55 +0500

    java

---
+++ index.js
@@ -0,0 +1,52 @@
+//js fundemental assignment no 1
+console.log( 'hello world ');
+alert( `this is sahar
+i am a student of class Web Development and mobile app development`);
+// no 2
+var birthYear = 1997
+alert(`type of variable birthYear is = ` +typeof birthYear);
+// no 3 move on html file
+// no 4 
+var age = prompt (`input your age`);
+var answer = "45";
+var days = 365;
+alert (`your age in days are` +age * 365);
+
+
+//no 5
+//var num = prompt(`write a number`);
+//var anotherNum = ++num
+//var total=anotherNum;
+//alert(total);
+
+//no 5 in function
+
+
+//var number;
+//function inc(num){
+   // number = ++num;
+    //return number}
+
+//alert(inc(5));
+
+//var number1;
+//function inc(num2){
+   // number1 = --num2;
+    //return number1}
+
+//alert(inc(4));
+
+
+//no 6
+//var num1 = prompt(`write a number`);
+//var anotherNum1 = --num1
+//var total1=anotherNum1;
+
+//alert(total1);
+
+// no 8
+
+
+
+
+
